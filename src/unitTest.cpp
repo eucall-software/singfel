b6d3b89ce1d0736f4a46b1d796c4870126bd5d75
Chun Hong Yoon <chun.hong.yoon@desy.de>
2014-04-24 13:35:52 +0200

    Added a poor man's unit test

---
+++ src/unitTest.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <iomanip>
+#include <sys/time.h>
+#include <armadillo>
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <gsl/gsl_spline.h>
+#include <gsl/gsl_errno.h>
+#include "detector.h"
+#include "beam.h"
+#include "particle.h"
+#include "diffraction.h"
+#include "toolbox.h"
+#include "diffraction.cuh"
+
+#include <algorithm>
+#include <boost/tokenizer.hpp>
+#include <boost/algorithm/string.hpp>
+
+using namespace std;
+using namespace arma;
+using namespace detector;
+using namespace beam;
+using namespace particle;
+using namespace diffraction;
+using namespace toolbox;
+
+//#define ARMA_NO_DEBUG
+
+#define USE_CUDA 0
+#define USE_CHUNK 0
+
+int main( int argc, char* argv[] ){
+
+	// Particle
+	CParticle particle = CParticle();
+	CBeam beam = CBeam();
+	CDetector det = CDetector();
+
+	typedef boost::tokenizer<boost::char_separator<char> > Tok;
+	
+	cout << "Successful!" << endl;
+
+  	return 0;
+}
+
