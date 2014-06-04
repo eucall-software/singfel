aed91085ec07ef0a9a721cfe81b3623bc47e1519
Chun Hong Yoon <chun.hong.yoon@desy.de>
2014-06-04 13:38:25 +0200

    write hdf5

--- libsingfel/io.h
+++ libsingfel/io.h
@@ -4,6 +4,11 @@
 #include <string>
 #include <hdf5.h>
 
+//#include <boost/tokenizer.hpp>
+//#include <boost/algorithm/string.hpp>
+
+#include <boost/filesystem.hpp>
+
 #ifndef H5_NO_NAMESPACE
 #ifndef H5_NO_STD
     using std::cout;
@@ -21,6 +26,239 @@
 
 using namespace std;
 using namespace arma;
+
+/*
+#include <fstream>
+bool fexists(const std::string filename) {
+  ifstream ifile(filename.c_str());
+  return ifile;
+}*/
+
+template<typename T> int hdf5writeT(std::string filename, std::string groupname, std::string subgroupname, std::string datasetname, T data, int appendDataset, int createSubgroup){
+	const H5std_string FILE_NAME( filename );
+	const H5std_string DATASET_NAME( datasetname );
+	
+	// TO DO: Use tokenizer to get groupname from datasetname
+	
+	int myDim1 = data.n_rows; // 6
+	int myDim2 = data.n_cols; // 10
+
+	// TO DO: Handle cubes and vectors
+	
+	const int MSPACE1_RANK = 1; // Rank of the first dataset in memory
+	const int MSPACE1_DIM = myDim1*myDim2; // Dataset size in memory
+	//const int MSPACE2_RANK = 1; // Rank of the second dataset in memory
+	//const int MSPACE2_DIM = 4; // Dataset size in memory
+	const int FSPACE_RANK = 2; // Dataset rank as it is stored in the file
+	const int FSPACE_DIM1 = myDim1; // Dimension sizes of the dataset as it is
+	const int FSPACE_DIM2 = myDim2; // stored in the file
+	//const int MSPACE_RANK = 2; // Rank of the first dataset in memory
+	//const int MSPACE_DIM1 = 8; // We will read dataset back from the file
+	//const int MSPACE_DIM2 = 9; // to the dataset in memory with these
+	// dataspace parameters
+	//const int NPOINTS = 4; // Number of points that will be selected
+	// and overwritten
+	int i,j; // loop indices */
+	 
+	/*
+	* Try block to detect exceptions raised by any of the calls inside it
+	*/
+	try
+	{
+	/*
+	* Turn off the auto-printing when failure occurs so that we can
+	* handle the errors appropriately
+	*/
+	Exception::dontPrint();
+	
+	H5File* file;
+	// Check if file exists, if not create a new file
+	if(appendDataset) {
+		std::cout << "file does exist" << std::endl;
+		file = new H5File(FILE_NAME, H5F_ACC_RDWR);
+	} else {
+		std::cout << "file does not exist" << std::endl;
+		// Create a file.
+		file = new H5File( FILE_NAME, H5F_ACC_TRUNC );
+	}
+/*
+	if ( !boost::filesystem::exists( filename ) ) {
+		std::cout << "file does not exist" << std::endl;
+		// Create a file.
+		file = new H5File( FILE_NAME, H5F_ACC_TRUNC );
+	} else {
+		std::cout << "file does exist" << std::endl;
+		file = new H5File(FILE_NAME, H5F_ACC_RDWR);
+	}
+*/
+
+	// Check if group exists
+	/*
+	* Access the group.
+	*/
+	Group* group;
+	try { // to determine if the dataset exists in the group
+		cout << " Trying to open group" << endl;
+		group = new Group( file->openGroup( groupname ));
+		cout << " Opened existing group" << endl;
+	}
+	catch( FileIException not_found_error ) {
+		cout << " Group not found." << endl;
+		// create group
+		group = new Group( file->createGroup( groupname ));
+		cout << " Group created." << endl;
+	}
+
+	if (createSubgroup) {
+		group = new Group( file->createGroup( subgroupname ));
+		cout << " Subgroup created." << endl;
+	}
+
+	if (!subgroupname.empty()){
+		group = new Group( file->openGroup( subgroupname ));
+		cout << " writing to subgroup." << endl;
+	}
+
+	/*
+	* Create property list for a dataset and set up fill values.
+	*/
+	int fillvalue = 0; /* Fill value for the dataset */
+	DSetCreatPropList plist;
+	if (typeid(data) == typeid(mat)) {
+		plist.setFillValue(PredType::NATIVE_DOUBLE, &fillvalue); // init with zeros
+	} else if (typeid(data) == typeid(fmat)) {
+    	plist.setFillValue(PredType::NATIVE_FLOAT, &fillvalue); // init with zeros
+	} else if (typeid(data) == typeid(imat)) {
+		plist.setFillValue(PredType::NATIVE_INT, &fillvalue); // init with zeros
+	} else if (typeid(data) == typeid(umat)) {
+		plist.setFillValue(PredType::NATIVE_UINT, &fillvalue); // init with zeros
+	}
+	/*
+	* Create dataspace for the dataset in the file.
+	*/
+	hsize_t fdim[] = {FSPACE_DIM1, FSPACE_DIM2}; // dim sizes of ds (on disk)
+	DataSpace fspace( FSPACE_RANK, fdim );
+	/*
+	* Create dataset and write it into the file.
+	*/
+	DataSet* dataset;
+	if (typeid(data) == typeid(mat)) {
+			dataset = new DataSet(file->createDataSet(
+	DATASET_NAME, PredType::NATIVE_DOUBLE, fspace, plist));
+	} else if (typeid(data) == typeid(fmat)) {
+    		dataset = new DataSet(file->createDataSet(
+	DATASET_NAME, PredType::NATIVE_FLOAT, fspace, plist));
+	} else if (typeid(data) == typeid(imat)) {
+			dataset = new DataSet(file->createDataSet(
+	DATASET_NAME, PredType::NATIVE_INT, fspace, plist));
+	} else if (typeid(data) == typeid(umat)) {
+			dataset = new DataSet(file->createDataSet(
+	DATASET_NAME, PredType::NATIVE_UINT, fspace, plist));
+	}
+
+	/*
+	* Select hyperslab for the dataset in the file, using 3x2 blocks,
+	* (4,3) stride and (2,4) count starting at the position (0,1).
+	*/
+	hsize_t start[2]; // Start of hyperslab
+	hsize_t stride[2]; // Stride of hyperslab
+	hsize_t count[2]; // Block count
+	hsize_t block[2]; // Block sizes
+	start[0] = 0; start[1] = 0;
+	stride[0] = 1; stride[1] = 1;
+	count[0] = myDim1; count[1] = myDim2; // how many unit cells (2x4)
+	block[0] = 1; block[1] = 1; // unit cell size (3x2)
+	fspace.selectHyperslab( H5S_SELECT_SET, count, start, stride, block);
+	/*
+	* Create dataspace for the first dataset.
+	*/
+	hsize_t dim1[] = {MSPACE1_DIM}; /* Dimension size of the first dataset 50
+	(in memory) */
+	DataSpace mspace1( MSPACE1_RANK, dim1 );
+	/*
+	* Select hyperslab.
+	* We will use 48 elements of the vector buffer starting at the
+	* second element. Selected elements are 1 2 3 . . . 48
+	*/
+	start[0] = 0;
+	stride[0] = 1;
+	count[0] = myDim1*myDim2;
+	block[0] = 1;
+	mspace1.selectHyperslab( H5S_SELECT_SET, count, start, stride, block);
+	/*
+	* Write selection from the vector buffer to the dataset in the file.
+	*/
+	
+	int counter = 0;
+	if (typeid(data) == typeid(mat)) {
+		double vector[MSPACE1_DIM]; // vector buffer for dset 50
+		for (i = 0; i < myDim1 ; i++){
+			for (j = 0; j < myDim2 ; j++){
+				vector[counter] = data(i,j); // write out as is
+				counter++;
+			}
+		}
+		dataset->write( vector, PredType::NATIVE_DOUBLE, mspace1, fspace );
+	} else if (typeid(data) == typeid(fmat)) {
+    	float vector[MSPACE1_DIM]; // vector buffer for dset 50
+		for (i = 0; i < myDim1 ; i++){
+			for (j = 0; j < myDim2 ; j++){
+				vector[counter] = data(i,j); // write out as is
+				counter++;
+			}
+		}
+		dataset->write( vector, PredType::NATIVE_FLOAT, mspace1, fspace );
+	} else if (typeid(data) == typeid(imat)) {
+		int vector[MSPACE1_DIM]; // vector buffer for dset 50
+		for (i = 0; i < myDim1 ; i++){
+			for (j = 0; j < myDim2 ; j++){
+				vector[counter] = data(i,j); // write out as is
+				counter++;
+			}
+		}
+		dataset->write( vector, PredType::NATIVE_INT, mspace1, fspace );
+	} else if (typeid(data) == typeid(umat)) {
+		unsigned int vector[MSPACE1_DIM]; // vector buffer for dset 50
+		for (i = 0; i < myDim1 ; i++){
+			for (j = 0; j < myDim2 ; j++){
+				vector[counter] = data(i,j); // write out as is
+				counter++;
+			}
+		}
+		dataset->write( vector, PredType::NATIVE_UINT, mspace1, fspace );
+	}
+	
+	/*
+	* Reset the selection for the file dataspace fid.
+	*/
+	fspace.selectNone();
+	/*
+	* Close the dataset and the file.
+	*/
+	delete dataset;
+	delete group;
+	delete file;
+	} // end of try block
+	// catch failure caused by the H5File operations
+	catch( FileIException error )
+	{
+	error.printError();
+	return -1;
+	}
+	// catch failure caused by the DataSet operations
+	catch( DataSetIException error )
+	{
+	error.printError();
+	return -1;
+	}
+	// catch failure caused by the DataSpace operations
+	catch( DataSpaceIException error )
+	{
+	error.printError();
+	return -1;
+	}
+	return 0;
+}
 
 template<typename T> T hdf5readT(std::string filename, std::string datasetname){
 	const H5std_string FILE_NAME( filename );
@@ -369,8 +607,8 @@ extern "C" {
 
 
 arma::fmat hdf5read(std::string,std::string);
+//int hdf5write(std::string,std::string,arma::fmat);
 int hdf5write(std::string,std::string,arma::fmat);
-int hdf5writeT(std::string,std::string,arma::fmat);
 
 arma::fmat load_asciiImage(std::string x);
 arma::fvec load_asciiEuler(std::string x);
