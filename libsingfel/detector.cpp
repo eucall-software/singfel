1072aeb369cd12f88581521fd4ec6bc3a6079179
Chun Hong Yoon <chun.hong.yoon@desy.de>
2014-12-05 15:00:27 +0100

    Added general polarization factor

--- libsingfel/detector.cpp
+++ libsingfel/detector.cpp
@@ -179,69 +179,43 @@ void CDetector::init_dp( beam::CBeam *beam ){
 	// Used for single particle. Set 1/pix_width=9090 for LCLS
 	q_xyz.zeros(py,px,3);
 	float rx, ry, r, twotheta, az;
-	float r_sq;
+	float pixDist, alpha;
 	solidAngle.zeros(py,px);
+	fmat twoTheta(py,px);
 	for (int ind_x = 0; ind_x < px; ind_x++) {
 		for (int ind_y = 0; ind_y < py; ind_y++) {
 			rx = (ind_x - cx) * pix_width; // equivalent to dividing by pixel resolution
 			ry = (ind_y - cy) * pix_width;
 			r = sqrt(pow(rx,2)+pow(ry,2));
 			twotheta = atan2(r,d);
+			twoTheta(ind_y,ind_x) = twotheta;
 			az = atan2(ry,rx);
 			q_xyz(ind_y,ind_x,0) = beam->get_wavenumber() * sin(twotheta)*cos(az);
 			q_xyz(ind_y,ind_x,1) = beam->get_wavenumber() * sin(twotheta)*sin(az);
 			q_xyz(ind_y,ind_x,2) = beam->get_wavenumber() * (cos(twotheta) - 1.0);
 			
-			r_sq =  pow(rx,2) + pow(ry,2) + pow(d,2); // real space
-			solidAngle(ind_y,ind_x) = pix_width * pix_height * cos(twotheta) / r_sq; // real space (Unitless)
-			
-			//if (ind_x==0 && ind_y==0) {
-			//cout << "cnx,cny,res: " << cx<<","<<cy<<","<<1./pix_width<<endl;
-			//cout << "fs,ss: " << ind_x<<","<<ind_y<<endl;
-			//cout << "cx,lambda,k: " << cx <<"," << beam->get_wavelength() << "," << beam->get_wavenumber() << "," << 1/beam->get_wavelength() << endl;
-			//cout << "rx,ry,r,twotheta: " <<rx<<","<<ry<<","<<r<<","<<twotheta<<","<<az<<","<<q_xyz(ind_y,ind_x,0)<<","<<q_xyz(ind_y,ind_x,1)<<","<<q_xyz(ind_y,ind_x,2)<<endl;
-			//}
+			// Analytical formula of solid angle (Zaluzec2014)
+			pixDist = sqrt(pow(rx,2) + pow(ry,2) + pow(d,2)); // distance from interaction to pixel center in real space
+			alpha = atan(pix_width/(2*pixDist));
+			solidAngle(ind_y,ind_x) = 4 * asin( pow(sin(alpha),2) );
 		}
 	}
 	q_mod = CToolbox::mag(q_xyz);
-//cout << "done" << endl;	
-/*		
-	q_xyz.zeros(py,px,3);
-	fmat r_x, r_y, k;
-	frowvec coord_x = (linspace<frowvec>(0.0, px-1, px) - cx) * pix_width;
-	fcolvec coord_y = (cy - linspace<fcolvec>(0.0, py-1, py)) * pix_height;
-	//cout << coord_x << endl;
-	double c2r = 1/(beam->lambda*d); // convert to reciprocal space 
-	fmat coord_x_mat = repmat(coord_x, py, 1);
-	fmat coord_y_mat = repmat(coord_y, 1, px);
-	r_x = coord_x_mat*c2r; // reciprocal coord
-	r_y = coord_y_mat*c2r;
-	//cout << "cx,cy: " << cx << "," << cy << endl;
-cout << "r_x r_y: " << r_x(0,0) << "," << r_y(0,0) << endl;
-	k = beam->k * ones<fmat>(py,px);
-	fmat c2e = k/(sqrt( pow(r_x,2) + pow(r_y,2) + pow(k,2) ));
-	q_xyz.slice(0) = r_x % c2e;
-	q_xyz.slice(1) = r_y % c2e;
-	q_xyz.slice(2) = k % (c2e - 1);
-	q_mod = CToolbox::mag(q_xyz);
-	// twotheta
-	fmat radial = sqrt(pow(coord_x_mat,2)+pow(coord_y_mat,2));
-	fmat twotheta;
-	twotheta.zeros(py,px);
-	for (int ind_x = 0; ind_x < px; ind_x++) {
-	for (int ind_y = 0; ind_y < py; ind_y++) {
-		twotheta(ind_y,ind_x) = atan2(radial(ind_y,ind_x),d);
-	}
-	}
-	fmat r_sq =  pow(coord_x_mat,2) + pow(coord_y_mat,2) + pow(d,2); // real space
-	solidAngle = pix_width * pix_height * cos(twotheta) / r_sq; // real space (Unitless)
-//cout << "solidAngle:" << solidAngle << endl;
-*/
-/******************THOMSON SCATTERING FIX HERE********************/
+
 	double re = 2.81793870e-15;			// classical electron radius (m)
-	// Vertical polarization, mu = pi/2, cos^2(mu) = 0
-	fmat tmp(py,px);
-	thomson = pow(re,2) * tmp.ones();
+	// Polarization factor, cos^2 mu + sin^2 mu * cos^2(2theta)
+	double mu; // polarization angle
+	string electricFieldPlane = "horizontal";
+	if (electricFieldPlane == "horizontal") {
+		mu = 0;
+	} else if (electricFieldPlane == "vertical") {
+		mu = datum::pi/2;
+	} else {
+		// unpolarized
+		mu = datum::pi/4;
+	}
+	fmat polarizationFactor = ( pow(cos(mu),2) + pow(sin(mu),2)*pow(cos(twoTheta),2) );
+	thomson = pow(re,2) * polarizationFactor;
 }
 
 void CDetector::set_param(Packet *x){
